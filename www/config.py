b26db6d675e392f86b47ab5dca48401ad3c4ffeb
Owen Zhang <owen.zhang@emc.com>
2018-07-16 14:50:25 +0800

    add sign and resigter page and logic

--- www/config.py
+++ www/config.py
@@ -6,47 +6,39 @@ from . import config_default
 
 class Dict(dict):
     '''
-    Simple dict but support access as x.y style
+    Simple dict but support access as x.y style.
     '''
     def __init__(self, names=(), values=(), **kw):
         super(Dict, self).__init__(**kw)
-        for k, v in zip(names,values):
+        for k, v in zip(names, values):
             self[k] = v
-
     def __getattr__(self, key):
         try:
             return self[key]
         except KeyError:
-            raise AttributeError(r"'Dict'object has no attribute '%s'" %key)
-
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
     def __setattr__(self, key, value):
         self[key] = value
-
-
 def merge(defaults, override):
     r = {}
     for k, v in defaults.items():
         if k in override:
             if isinstance(v, dict):
-                r[k] = merge(v,override[k])
+                r[k] = merge(v, override[k])
             else:
                 r[k] = override[k]
         else:
             r[k] = v
     return r
-
 def toDict(d):
-    D = dict()
-    for k,v in d.items():
-        D[k] = toDict(v) if isinstance(v,dict) else v
+    D = Dict()
+    for k, v in d.items():
+        D[k] = toDict(v) if isinstance(v, dict) else v
     return D
-
 configs = config_default.configs
-
 try:
     import config_override
     configs = merge(configs, config_override.configs)
 except ImportError:
     pass
-
 configs = toDict(configs)
